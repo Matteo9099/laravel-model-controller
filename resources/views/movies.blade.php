6966e57cb32774a45e1df9c2841b4d84e2b6eb41
Matteo Morganti <matteo99morganti@gmail.com>
2022-03-30 18:18:09 +0200

    Commit

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -5,25 +5,27 @@
 
 @section('content')
 
-    @foreach ($movies as $movie)
-            
-        <div class="card"> 
-            {{-- titolo --}}
-            <div class="title-item">
-                <h3>Titolo: {{ $movie->title }}</h3>
-            </div>
-            {{-- tabella info --}}
-            <div class="table-card">
-                
-                <p class="underline">Tiotolo originale: {{ $movie->original_title }}</p>
-                <p class="underline">Nazionalità: {{ $movie->nationality }}</p>
-                <p class="underline">Data di uscita: {{ $movie->date }}</p>
-                <p class="underline">Voto: {{ $movie->vote }}</p>
-              
-            </div>
-            <br>
-        </div>
+        <div class="container">
 
-    @endforeach
+            @foreach ($movies as $movie)
+            
+            <div class="card"> 
+                {{-- titolo --}}
+                <div class="title-item">
+                    <h3>Titolo: {{ $movie->title }}</h3>
+                </div>
+                {{-- tabella info --}}
+                <div class="table-card">
+                    <p class="underline">Tiotolo originale: {{ $movie->original_title }}</p>
+                    <p class="underline">Nazionalità: {{ $movie->nationality }}</p>
+                    <p class="underline">Data di uscita: {{ $movie->date }}</p>
+                    <p class="underline">Voto: {{ $movie->vote }}</p>
+                </div>
+                <br>
+            </div>
+            
+            @endforeach
+
+        </div>
 
 @endsection
